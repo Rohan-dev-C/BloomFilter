39cb64057641e0e5f0e5ad3ddb7a44fec51da195
Rohan.Sampath <Rohan.Sampath@GMR-118-2-2>
2023-02-04 14:17:04 -0800

    Add project files.

---
+++ BloomFilter/BloomFilter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloomFilter
+{
+    public class BloomFilter<T>
+    {
+        int m; 
+        HashSet<Func<T, int>> set;
+
+        bool[] bits;
+
+        public BloomFilter(int cap)
+        {
+            set= new HashSet<Func<T, int>>();
+            LoadHashFunc(HashFuncOne);
+            LoadHashFunc(HashFuncTwo);
+            LoadHashFunc(HashFuncThree);
+
+            bits = new bool[cap];
+            m = cap; 
+        }
+
+        public void LoadHashFunc(Func<T, int> hashFunc)
+        {
+           set?.Add(hashFunc);
+        }
+
+        public void Insert(T item, int k)
+        {
+            foreach (var temp in set)
+            {
+                var temp2 = temp?.Invoke(item);
+                bits[Math.Abs(temp2.Value % bits.Length)] = true; 
+            } 
+           
+        }
+
+        public bool ProbablyContains(T item)
+        {
+            foreach (var temp in set)
+            {
+                var temp2 = temp?.Invoke(item);
+                if(bits[Math.Abs(temp2.Value % bits.Length)] == false)
+                {
+                    return false; 
+                }
+            }
+            return true; 
+        }
+
+        private int HashFuncOne(T item)
+        {
+            return item.GetHashCode(); 
+        }
+
+        private int HashFuncTwo(T item)
+        {
+            return item.GetHashCode() + item.ToString().GetHashCode(); 
+        }
+
+        private int HashFuncThree(T item)
+        {
+            return (HashFuncOne(item) + HashFuncTwo(item)) * "dog".GetHashCode(); 
+        }
+    }
+}
---
+++ BloomFilter/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace BloomFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BloomFilter<string> bloomFilter = new BloomFilter<string>(5000);
+
+            bloomFilter.Insert("red", 3);
+            bloomFilter.Insert("orange", 3);
+            bloomFilter.Insert("yellow", 3);
+            bloomFilter.Insert("green", 3);
+            bloomFilter.Insert("blue", 3);
+            bloomFilter.Insert("purple", 3);
+
+            var temp = bloomFilter.ProbablyContains("pink");
+            var temp2 = bloomFilter.ProbablyContains("yellow");
+        }
+    }
+}
